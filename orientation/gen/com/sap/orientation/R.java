1f1a5c6d9d587f27595124f65379a7a909f8f5fb
Joseph Hou <hou.joseph@gmail.com>
2013-08-24 03:56:47 -0700

    merged

--- orientation/gen/com/sap/orientation/R.java
+++ orientation/gen/com/sap/orientation/R.java
@@ -29,6 +29,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int intern_app_icon=0x7f020001;
         public static final int map=0x7f020002;
+        public static final int map_tiny=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f090007;
---
+++ orientation/src/com/sap/orientation/backupMapSectionFragment.java
@@ -0,0 +1,46 @@
+package com.sap.orientation;
+
+public class backupMapSectionFragment {
+
+}
+
+/**
+
+package com.sap.orientation;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class MapSectionFragment extends Fragment {
+
+	
+	private static ImageView image1;
+	
+	/**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */ /*
+	public static final String ARG_SECTION_NUMBER = "section_number";
+
+	public MapSectionFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_map, container,
+				false);
+		image1 = (ImageView) rootView.findViewById(R.id.imageView1);
+		image1.setImageResource(R.drawable.map);
+
+		return rootView;
+	}
+}
+
+
+*/
